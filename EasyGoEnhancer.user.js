35e90289c057e857f43d4f5ea211dfdb1b30e5b5
Junjia Ni <creamidea@gmail.com>
2016-11-16 23:28:45 +0800

    获取数据直接填充

--- EasyGoEnhancer.user.js
+++ EasyGoEnhancer.user.js
@@ -11,42 +11,63 @@
 
 // ==/UserScript==
 
-(function() {
+function getTodoList ($content) {
+    return Array.prototype.map.call($content, function ($tabContent, index) {
+        var $table = $tabContent.children[0].children[0];
+        if ($table === undefined) return;
+        return Array.prototype.map.call(
+            $table.querySelectorAll('tr'),
+            function ($tr, i) {
+                if (i>0) {
+                    return $tr;
+                }
+            }).filter(function(a){if (a) return true;});
+    }).filter(function(a){if (a) return true;});
+}
+
+function DoIt() {
     'use strict';
 
-	// 删除首页中“待办事项”后面的伪Tags：
-	var FakeTags = document.querySelector(".NewTitle1");
-	for (var i = 5 - 1; i >= 0; i--) {
-		FakeTags.removeChild(FakeTags.children[1]);
-	}
+    // 删除首页中“待办事项”后面的伪Tags：
+    var FakeTags = document.querySelector('.NewTitle1');
+    for (var i = 4; i >= 0; i--) {
+        FakeTags.removeChild(FakeTags.children[1]);
+    }
 
-	// 增加容器，并插入到“待办事项”标题下:
-	// 以下为为了使用 insertBefore 而进行定位查找相关元素：
-	var MaoList = document.querySelector(".contPage");
-	var MaoTitle = document.querySelector(".newfooter");
+    // 增加容器，并插入到“待办事项”标题下:
+    // 以下为为了使用 insertBefore 而进行定位查找相关元素：
+    var MaoList = $(".contPage");
+    var MaoTitle = $(".newfooter");
 
-	for (var m = 0; m <= 5-1; m++) {
-		// 新建一个div容器：
-		var ToDoList = document.createElement('div');
-		var PerId ="ToDoList" + m.toString();
-		ToDoList.id = PerId;
-		ToDoList.innerText = "Mao is trying to downloading ......";
+    var todoUrl = "http://www.ascendacpa.com.cn/MoreTask3.aspx?ind=1";
+    $.get(todoUrl, function (content) {
+        var $content = $(content).find('[id*=tabContent__]');
+        var todoList = getTodoList($content);
+        var docfrag = document.createDocumentFragment();
+        var div$ = document.createElement('div');
+        var table$ = document.createElement('table');
+        var tbody$ = document.createElement('tbody');
 
-		// 把div容器插入到大标题“待办事项”下：
-		MaoList.insertBefore(ToDoList, MaoTitle);
+        docfrag.appendChild(div$);
+        div$.appendChild(table$);
+        table$.appendChild(tbody$);
 
-		var ifEmpty = "";
-		var JPerId = "#" + PerId;
-		var PerUrl = "http://www.ascendacpa.com.cn/MoreTask3.aspx " + "#tabContent__" + m.toString() + " div";
+        Array.prototype.map.call(todoList, function(todo) {
+            Array.prototype.map.call(todo, function(item) {
+                tbody$.appendChild(item);
+            });
+        });
+        FakeTags.parentElement.insertBefore(docfrag, FakeTags.nextElementSibling);
+    });
 
-		$(JPerId).load(PerUrl, function () {
-			// 判断是否为空，如果为空，则不用显示脚注“总记录”的条数
-			ifEmpty = document.getElementById(PerId).querySelector("div").innerText;
-			if(ifEmpty === ""){
-				//利用querySelector定位到有id属性的第一个div
-				var record = document.querySelector(JPerId);
-				record.removeChild(record.children[1]);
-			}
-		});
-	}
+    return;
+}
+(function() {
+    var btn = document.createElement('button');
+    btn.onclick = DoIt;
+    btn.innerHTML = 'Do It';
+    btn.style.position = 'fixed';
+    btn.style.top = 0;
+    btn.style.right = 0;
+    document.body.appendChild(btn);
 })();
